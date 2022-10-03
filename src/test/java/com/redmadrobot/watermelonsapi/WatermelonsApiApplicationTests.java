1a4fa8eb7a418abdaa872828014717a9514c2ebb
Ildar <ildarthegreat@gmail.com>
2022-10-03 21:32:45 +0400

    init commit

---
+++ src/test/java/com/redmadrobot/watermelonsapi/WatermelonsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmadrobot.watermelonsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatermelonsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
